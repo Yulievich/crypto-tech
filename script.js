c9670a41df574bea91f52f7d7facd402712d07ae
Lisyuk Vladislav <84083279+Yulievich@users.noreply.github.com>
2023-02-27 16:22:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+VANTA.NET({
+    el: "#animation",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3f6cff,
+    backgroundColor: 0x80311,
+    points: 17.00,
+    spacing: 17.00
+})
